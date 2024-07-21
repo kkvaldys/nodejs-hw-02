bd29bb7780cdbfb6fbbf1bc0d6ce6c481b5b04b7
kkvaldys <vladbigun677@gmail.com>
2024-07-21 13:17:15 +0300

    completed

---
+++ src/db/models/Contact.js
@@ -0,0 +1,42 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: false,
+        },
+        isFavourite: {
+            type: Boolean,
+            default: false,
+        },
+        contactType: {
+            type: String,
+            enum: ['work', 'home', 'personal'],
+            default: 'personal',
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import initMongoConnection from "./db/initMongoDB.js";
+import {startServer} from "./server.js";
+const bootstrap = async()=>{
+    await initMongoConnection();
+    startServer();
+}
+
+bootstrap();
